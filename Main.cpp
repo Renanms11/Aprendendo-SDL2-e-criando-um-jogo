3719a78230ca5350b997ce58eea0ed0ea17514f9
RenanSena2023 <renansena20152proton.me>
2025-09-08 20:42:38 -0300

    Adicionando imagem ao fundo e separando o codigo em funções : criar janela, carregando a imagem , fechando a janela e a surpeficie da janela

--- Main.cpp
+++ Main.cpp
@@ -5,51 +5,97 @@
 const int SCREEN_WIDTH=640;
 const int SCREEN_HEIGHT = 580;
 
-int main(int argc, char* argv[]){
-    //criando os ponteiros da janela
-    SDL_Window* window = NULL;
-    //declarando a variavel da superficie
-    SDL_Surface* screenSurface = NULL;
+SDL_Window* gWindow = NULL;
+SDL_Surface* gSreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
 
-    //iniciando o sdl, so com video e fazendo um if para garantir o funcionamento
-    if(SDL_Init(SDL_INIT_VIDEO) <0){
-       printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+bool init();
+bool loadMedia();
+void close();
+
+
+bool init(){
+   
+    bool sucess = true;
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        printf("Problemas para iniciar o SDL SDL_ERROR:%s\n",SDL_GetError());
+        sucess = false;
     }else{
-        // instanciando a janela, nome,posição x, posição y, largura,altura , modo mostrar
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, 
-            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-    // veriaficando se a janela foi criada para pintar na surpeficie
-        if(window == NULL){
-            printf("window could not be create! SDL_Error: %s\n",SDL_GetError());
+        gWindow = SDL_CreateWindow("Argonauts",SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,SCREEN_WIDTH,SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
+        
+        if(gWindow == NULL){
+            printf("Problemas para iniciar janela !! SDL_ERROR:%s\n",SDL_GetError());
+                    sucess = false;
+
         }else{
-            //instanciando a surpeficie da janela
-            screenSurface = SDL_GetWindowSurface(window);
-
-            //pintando a superficie de branco
-            SDL_FillRect(screenSurface,NULL, SDL_MapRGB(screenSurface ->format,255,255,0));
-            //atualizando a superficie.
-            SDL_UpdateWindowSurface(window);
-           SDL_Event e;
-            bool quit = false; 
-            while( quit == false ){ 
-                while( SDL_PollEvent( &e ) ){ 
-                    if( e.type == SDL_QUIT ) quit = true; 
-                    } 
-                }
+                    
+            gSreenSurface = SDL_GetWindowSurface(gWindow);
         }
-        //destruindo a janela
-        SDL_DestroyWindow(window);
-        //fechando o subsistema do sdl
-        SDL_Quit();
+    }
+        return sucess;
 
-        return 0;
     }
 
+bool loadMedia(){
+    bool sucess = true;
 
-
-
-
-
-
-
+    //carrear imagem inicial
+    gHelloWorld = SDL_LoadBMP("./assets/hello_world.bmp");
+    if(gHelloWorld == NULL){
+        printf("Problemas para carregar a imagem !! SDL_ERROR:%s\n",SDL_GetError());
+        sucess= false;
+    }
+   
+    return sucess;
 }
+
+void close(){
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = NULL;
+
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+
+    SDL_Quit();
+} 
+
+int main(int argc, char* argv[]){
+
+    if(!init()){
+        printf("Fala ao inicializar!\n");
+    }else{
+        if(!loadMedia()){
+            printf( "Falha ao carregar mídia!\n" );
+
+        }else{
+            SDL_BlitSurface( gHelloWorld, NULL, gSreenSurface, NULL);
+            SDL_UpdateWindowSurface(gWindow);
+
+            SDL_Event e;
+            bool quit = false;
+
+            while(quit == false){
+                while( SDL_PollEvent( &e )) {
+                    if(e.type == SDL_QUIT){
+                        quit = true;
+                    }
+                }
+            }
+
+        }
+    }
+    
+    close();
+
+    return 0;
+ }
+
+
+
+
+
+
+
+
